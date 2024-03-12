d0d8519967144b2370edd46174c84249ba31f3c3
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2024-03-12 16:42:15 +0100

    Fixed typo

--- Mainwindow.cpp
+++ Mainwindow.cpp
@@ -964,7 +964,7 @@ void MainWindow::on_actionHelpManual_triggered() {
         openURL = "Manual_EN.pdf";
 
     if(!QFile::exists(openURL)) {
-        QMessageBox::critical(this, tr("Manual not found"), tr("<p>The file was not found:</p><p><b>%1<b></p>").arg(openURL));
+        QMessageBox::critical(this, tr("Manual not found"), tr("<p>The file was not found:</p><p><b>%1</b></p>").arg(openURL));
         return;
     }
     QDesktopServices::openUrl(QUrl(openURL));
