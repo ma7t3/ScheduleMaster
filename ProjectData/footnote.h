5918f1a3ff9f54888c9d29d41ce77c8d5acaa3bd
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2024-03-30 00:14:44 +0100

    Added name to Footnote

--- ProjectData/footnote.h
+++ ProjectData/footnote.h
@@ -8,21 +8,26 @@
 class Footnote : public virtual ProjectDataItem {
     Q_OBJECT
 public:
-    Footnote(QObject *parent, const QString &id, const QString &identifier, const QString &description = "");
+    Footnote(QObject *parent, const QString &id, const QString &identifier, const QString &name, const QString &description = "");
     Footnote(const Footnote &);
     Footnote operator=(const Footnote &);
+    bool operator<(const Footnote &);
 
     QString identifier() const;
     void setIdentifier(const QString &newIdentifier);
 
+    QString name() const;
+    void setName(const QString &newName);
+
     QString description() const;
     void setDescription(const QString &newDescription);
+
 
 protected:
     void copy(const Footnote &);
 
 private:
-    QString _identifier, _description;
+    QString _identifier, _name, _description;
 };
 
 #endif // FOOTNOTE_H
