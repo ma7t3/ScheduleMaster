6f042834115ecc7c57d85c39c277dc525ff7921d
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2024-06-08 21:35:00 +0200

    implemented ProjectDataModel::updating()

--- DataModels/ProjectDataModel.h
+++ DataModels/ProjectDataModel.h
@@ -8,26 +8,29 @@
 template<typename T>
 class ProjectDataModel : public ProjectDataModelSignals {
 public:
-    explicit ProjectDataModel(QObject *parent) : ProjectDataModelSignals(parent) {}
+    explicit ProjectDataModel(QObject *parent) : ProjectDataModelSignals(parent), _updating(false) {}
     virtual  QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override = 0;
     virtual  int rowCount(const QModelIndex &parent = QModelIndex())                                   const override = 0;
     virtual  int columnCount(const QModelIndex &parent = QModelIndex())                                const override = 0;
     virtual  QVariant data(const QModelIndex &index, int role = Qt::DisplayRole)                       const override = 0;
+             int        indexOf(T *t)                                                                  const          {return _items.indexOf(t);}
              int        itemCount()                                                                    const          {return ProjectDataModel<T>::_items.count(); }
-             // T *        itemAt(const QModelIndex &index)                                               const          {return itemAt(index.row());}
              T *        itemAt(const int &index)                                                       const          {return ProjectDataModel<T>::_items[index];}
              QList<T *> itemList()                                                                     const          {return _items;}
+             bool       updating()                                                                     const          {return _updating;}
 
 public slots:
-    virtual void setSearchFilter(const QString &) = 0;
-    virtual void addItems(const QList<T *>)       = 0;
-    virtual void changeItems(const QList<T *>)    = 0;
-    virtual void removeItems(const QList<T *>)    = 0;
+    virtual void setSearchFilter(const QString &)  = 0;
+    virtual void addItems       (const QList<T *>) = 0;
+    virtual void changeItems    (const QList<T *>) = 0;
+    virtual void removeItems    (const QList<T *>) = 0;
     void reset() {
+        _updating = true;
         QAbstractTableModel::beginResetModel();
-        ProjectDataModel<T>::_items.clear();
-        ProjectDataModel<T>::_items = fetchData();
+        _items.clear();
+        _items = fetchData();
         QAbstractTableModel::endResetModel();
+        _updating = false;
         emit ProjectDataModelSignals::updateFinished();
     }
 
@@ -38,6 +41,8 @@ protected:
 
     QList<T *> _items;
     QString    _search;
+
+    bool _updating;
 };
 
 #endif // PROJECTDATAMODEL_H
