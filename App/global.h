149fd68b4e89171ae74e6f9e803cf6cca136067b
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2024-08-05 17:59:44 +0200

    global.h cleanup

--- App/global.h
+++ App/global.h
@@ -1,22 +1,10 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
-
-#include <QUuid>
 #include <QColor>
 
 class global {
-
 public:
-    static const int releaseType = 1;
-    static const int primaryVersion = 2;
-    static const int secondaryVersion = 1;
-    static const int tertiaryVersion = 0;
-
-    static QString getNewID() {
-        return QUuid::createUuid().toString(QUuid::WithoutBraces);
-    }
-
     static QColor getContrastColor(QColor c) {
         c = c.toHsl();
         if(c.lightness() >= 128)
