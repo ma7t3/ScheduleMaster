766d39ac765d8345b54c303f017bafd467309d0c
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2025-01-19 23:37:31 +0100

    add busstops in ProjectData

--- ProjectData/ProjectData.cpp
+++ ProjectData/ProjectData.cpp
@@ -26,3 +26,36 @@ void ProjectData::reset() {
     _undoStack->clear();
     _filePath = "";
 }
+
+int ProjectData::busstopCount() const {
+    return _busstops.count();
+}
+
+Busstop *ProjectData::busstop(const QUuid &id) {
+    return _busstops.contains(id) ? _busstops[id] : nullptr;
+}
+
+Busstop *ProjectData::findBusstopByName(const QString &name) {
+    auto it = std::find_if(_busstops.begin(), _busstops.end(),
+                           [name](Busstop* busstop) {
+                               return busstop->name() == name;
+                           });
+
+    return (it != _busstops.end()) ? it.value() : nullptr;
+}
+
+QList<Busstop *> ProjectData::busstops() const {
+    return _busstops.values();
+}
+
+void ProjectData::addBusstop(Busstop *busstop) {
+    _busstops.insert(busstop->id(), busstop);
+}
+
+void ProjectData::removeBusstop(Busstop *busstop) {
+    removeBusstop(busstop->id());
+}
+
+void ProjectData::removeBusstop(const QUuid &id) {
+    _busstops.remove(id);
+}
--- ProjectData/ProjectData.h
+++ ProjectData/ProjectData.h
@@ -3,6 +3,9 @@
 
 #include <QObject>
 #include <QUndoStack>
+#include <QUuid>
+
+#include "Busstop.h"
 
 class ProjectData : public QObject {
     Q_OBJECT
@@ -17,11 +20,21 @@ public:
 
     void reset();
 
+    int busstopCount() const;
+    Busstop *busstop(const QUuid &id);
+    Busstop *findBusstopByName(const QString &name);
+    QList<Busstop *> busstops() const;
+    void addBusstop(Busstop *);
+    void removeBusstop(Busstop *);
+    void removeBusstop(const QUuid &id);
+
 signals:
 
 private:
     QString _filePath;
     QUndoStack *_undoStack;
+
+    QMap<QUuid, Busstop *> _busstops;
 };
 
 #endif // PROJECTDATA_H
