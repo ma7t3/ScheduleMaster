9e511db112d6c6dc9e95d9455322cf27a95a7f4b
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2025-04-18 19:03:59 +0200

    ActionShortcutMapper docs

--- Global/ActionShortcutMapper.h
+++ Global/ActionShortcutMapper.h
@@ -7,40 +7,75 @@
 
 #include "LocalConfig.h"
 
+/**
+ * @brief The ActionShortcutMapper class is used to connect QActions with their corresponding keyboard shortcuts.
+ *
+ * Since ScheduleMaster supports custom shortcuts that can be changed by the user and should update at runtime without restarting the application, an additional logic is needed to handle this.
+ *
+ * The class listens to the LocalConfigs::keyboardShortcutChanged signal and updates the action's shortcut accordingly.
+ */
 class ActionShortcutMapper : public QObject {
     Q_OBJECT
 private:
+    /// Private constructor for the signleton instance
     explicit ActionShortcutMapper();
+
+    /// Private destructor for the signleton instance
     ~ActionShortcutMapper();
 
 public:
-    /// Initializes the ActionShortcutMapper class. Connects the LocalConfigs::keyboardShortcutChanged signal to the ActionShortcutMapper::onActionShortcutChanged slot.
+    /**
+     * Initializes the ActionShortcutMapper class.
+     *
+     * This connects the LocalConfigs::keyboardShortcutChanged signal to the ActionShortcutMapper::onActionShortcutChanged slot.
+     */
     static void init();
 
     /// Returns the singleton instance of the ActionShortcutMapper class.
     static ActionShortcutMapper *instance();
 
-    /// Maps an action to the given shortcut ID.
+    /**
+     * @brief Maps an action to the given shortcut ID.
+     * @param action The QAction pointer
+     * @param shortcutID The shortcut ID that should be mapped to the action
+     */
     static void map(QAction *action, const QString &shortcutID);
 
-    /// Removes an action from the ActionShortcutMapper.
+    /**
+     * @brief Removes an action from the ActionShortcutMapper.
+     * @param action The QAction pointer to remove
+     */
     static void remove(QAction *action);
 
     ActionShortcutMapper& operator=(const ActionShortcutMapper&) = delete;
     ActionShortcutMapper(const ActionShortcutMapper&) = delete;
 
 protected slots:
-    /// Updates an existing action with a new shortcutID
+    /**
+     * @brief Updates an existing action with a new shortcutID
+     * @param action The QAction pointer to update
+     * @param shortcutID The new shortcut ID to apply to action
+     */
     static void update(QAction *action, const QString &shortcutID);
 
-    /// Handles the destruction of an action. Removes the action from mapper
+    /**
+     * @brief Handles the destruction of an action. Removes the action from mapper
+     * @param obj The QObject pointer to the action that was destroyed
+     */
     static void onActionDestroyed(QObject *obj);
 
-    /// Handles the change of a keyboard shortcut. Updates the action's shortcut.
+    /**
+     * @brief Handles the change of a keyboard shortcut. Updates the action's shortcut.
+     * @param id The shortcut ID
+     * @param keySequence The new key sequence that is associated with the ID
+     */
     static void onActionShortcutChanged(const QString &id, const QKeySequence &keySequence);
 
 private:
+    /// Maps a list of shortcut IDs to their corresponding actions.
     static inline QHash<QString, QList<QAction *>> _shortcutActionsHash;
+
+    /// Maps every single action to its shortcut ID.
     static inline QHash<QAction *, QString> _actionShortcutHash;
 };
 
