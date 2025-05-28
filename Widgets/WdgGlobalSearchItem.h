02cc9818077bb490864d7b48b60d907f521a9084
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2025-05-29 01:55:49 +0200

    Improve layout of global search results

---
+++ Widgets/WdgGlobalSearchItem.h
@@ -0,0 +1,25 @@
+#ifndef WDGGLOBALSEARCHITEM_H
+#define WDGGLOBALSEARCHITEM_H
+
+#include "Global/ActionManager.h"
+
+#include <QWidget>
+
+namespace Ui {
+class WdgGlobalSearchItem;
+}
+
+class WdgGlobalSearchItem : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit WdgGlobalSearchItem(const ActionConfig &actionConfig, QWidget *parent = nullptr);
+    ~WdgGlobalSearchItem();
+
+    void setAction(const ActionConfig &actionConfig);
+
+private:
+    Ui::WdgGlobalSearchItem *ui;
+};
+
+#endif // WDGGLOBALSEARCHITEM_H
