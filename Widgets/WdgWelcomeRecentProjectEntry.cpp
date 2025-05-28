ae86e2c895b8b2f7a4f1dbdd4fdd4d7567956bd3
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2025-05-28 20:15:14 +0200

    Welcome Dock uses ActionController/IconController now

--- Widgets/WdgWelcomeRecentProjectEntry.cpp
+++ Widgets/WdgWelcomeRecentProjectEntry.cpp
@@ -1,29 +1,40 @@
 #include "WdgWelcomeRecentProjectEntry.h"
 #include "ui_WdgWelcomeRecentProjectEntry.h"
 
+#include "Global/ActionController.h"
+#include "Global/IconController.h"
+
 #include <QDateTime>
 #include <QStyleHints>
 #include <QStyle>
+#include <QPainter>
 
 WdgWelcomeRecentProjectEntry::WdgWelcomeRecentProjectEntry(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::WdgWelcomeRecentProjectEntry) {
+    ui(new Ui::WdgWelcomeRecentProjectEntry),
+    _fileExists(true) {
     ui->setupUi(this);
-
-    ui->lIcon->setScaledContents(true);
 
     setupLastUsedLabel();
 
     connect(ui->pbOpen,   &QPushButton::clicked, this, [this](){emit open(path());});
     connect(ui->pbRemove, &QPushButton::clicked, this, [this](){emit removeFromList(path());});
+
+    ActionController::add(ui->pbOpen, "project.recentFiles.openItem", ActionController::IconComponent);
+    ActionController::add(ui->pbRemove, "project.recentFiles.removeItem", ActionController::IconComponent);
+
+    connect(IconController::instance(), &IconController::currentIconSetChanged, this, &WdgWelcomeRecentProjectEntry::updateIcon);
+    updateIcon();
 }
 
 WdgWelcomeRecentProjectEntry::~WdgWelcomeRecentProjectEntry() {
     delete ui;
 }
 
-void WdgWelcomeRecentProjectEntry::setIcon(const QPixmap &pixmap) {
-    ui->lIcon->setPixmap(pixmap);
+void WdgWelcomeRecentProjectEntry::paintEvent(QPaintEvent *e) {
+    QPainter painter(this);
+    int margin = (sizeHint().height() - 48) / 2;
+    painter.drawPixmap(margin, margin, _icon.pixmap(_icon.actualSize(QSize(48, 48))));
 }
 
 void WdgWelcomeRecentProjectEntry::setName(const QString &newName) {
@@ -39,7 +50,8 @@ void WdgWelcomeRecentProjectEntry::setLastUsed(const QDateTime &newLastUsed) {
 }
 
 void WdgWelcomeRecentProjectEntry::setFileMissing() {
-    ui->lIcon->setPixmap(QPixmap(":/Icons/classic/file-close.ico"));
+    _fileExists = false;
+    updateIcon();
     ui->lLastUsed->setText(tr("This file no longer exists."));
     ui->lName->setDisabled(true);
     ui->lPath->setDisabled(true);
@@ -60,4 +72,9 @@ void WdgWelcomeRecentProjectEntry::setupLastUsedLabel() {
 
     // update if the colorscheme changes at runtime
     connect(qApp->styleHints(), &QStyleHints::colorSchemeChanged, this, [this](){setupLastUsedLabel();});
+}
+
+void WdgWelcomeRecentProjectEntry::updateIcon() {
+    _icon = IconController::icon(_fileExists ? "file" : "file-close");
+    update();
 }
