7d3bab0c8d600031e72f91f1f65e01351fd8cdec
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2025-05-30 10:35:13 +0200

    Remove global dock toggle actions
    These actions are now created dynamically from the dock config

--- Global/DockManager.cpp
+++ Global/DockManager.cpp
@@ -1,12 +1,27 @@
 #include "DockManager.h"
 
+#include "Global/Global.h"
+#include "Global/ActionManager.h"
+
 DockConfig::DockConfig(const QJsonObject &jsonObject, const int &index) : GlobalConfigItem(jsonObject, index) {
     name = jsonObject.value("name").toString();
     icon = jsonObject.value("icon").toString();
+    defaultKeyboardShortcut = Global::parseKeyboardShortcutConfigString(jsonObject.value("defaultKeyboardShortcut"));
 }
 
 DockConfig::DockConfig(const QString &id, const int &index) : GlobalConfigItem(id, index) {}
 
 DockManager::DockManager(QObject *parent) : GlobalConfigManager(parent) {
     loadItems("Docks");
+
+    for(DockConfig &dock : items()) {
+        ActionConfig action(QString("view.docks.%1.toggle").arg(dock.id()));
+        action.text                    = dock.name;
+        action.description             = tr("Show/hide dock: %1").arg(dock.name);
+        action.icon                    = dock.icon;
+        action.breadcrumb              = {tr("View"), tr("Workspaces")};
+        action.canHaveShortcut         = true;
+        action.defaultKeyboardShortcut = dock.defaultKeyboardShortcut;
+        ActionManager::addItem(action);
+    }
 }
