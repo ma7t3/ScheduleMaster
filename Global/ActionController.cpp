bc0d630c3d44459396013c34d68677dd05dbe2ed
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2025-06-05 14:19:32 +0200

    ActionController: refactoring & documentation

--- Global/ActionController.cpp
+++ Global/ActionController.cpp
@@ -2,38 +2,34 @@
 
 #include "Global/StyleHandler.h"
 
-void GlobalActionWrapper::execute() {
-    QAction *action = qobject_cast<QAction *>(widget);
-    QAbstractButton *button = qobject_cast<QAbstractButton *>(widget);
-
-    if (action)
-        action->trigger();
-
-    if (button)
-        button->click();
-}
 GlobalActionWrapper::GlobalActionWrapper(QObject *widget) : widget(widget) {
-    if (widget) {
+    if(widget) {
         QAction *action = qobject_cast<QAction *>(widget);
         QAbstractButton *button = qobject_cast<QAbstractButton *>(widget);
 
-        if (action)
+        if(action)
             shortcut = action->shortcut();
 
-        if (button)
+        if(button)
             shortcut = button->shortcut();
     }
 }
 
+void GlobalActionWrapper::execute() {
+    QAction *action = qobject_cast<QAction *>(widget);
+    QAbstractButton *button = qobject_cast<QAbstractButton *>(widget);
 
-ActionController::ActionController(QObject *parent) : QObject(parent) {
-    connect(IconController::instance(), &IconController::currentIconSetChanged, this, &ActionController::onIconSetChanged);
-    connect(StyleHandler::instance(), &StyleHandler::styleChanged, this, &ActionController::onIconSetChanged);
-    connect(ActionManager::instance(), &ActionManager::keyboardShortcutChanged, this, &ActionController::onActionShortcutChanged);
+    if(action)
+        action->trigger();
+
+    if(button)
+        button->click();
 }
 
 void ActionController::init() {
-    instance();
+    connect(IconController::instance(), &IconController::currentIconSetChanged,  ActionController::instance(), &ActionController::onIconSetChanged);
+    connect(StyleHandler::instance(),   &StyleHandler::styleChanged,             ActionController::instance(), &ActionController::onIconSetChanged);
+    connect(ActionManager::instance(),  &ActionManager::keyboardShortcutChanged, ActionController::instance(), &ActionController::onActionShortcutChanged);
 }
 
 ActionController *ActionController::instance() {
@@ -41,19 +37,26 @@ ActionController *ActionController::instance() {
     return &instance;
 }
 
-QAction *ActionController::createAction(const QString &actionID, const ActionComponents &components, QObject *parent) {
+QAction *ActionController::createAction(const QString &actionID, const ActionComponents &components,
+                                        QObject *parent) {
     return add(new QAction(parent), actionID, components);
 }
 
-QPushButton *ActionController::createPushButton(const QString &actionID, const ActionComponents &components, QWidget *parent) {
+QPushButton *ActionController::createPushButton(const QString &actionID,
+                                                const ActionComponents &components,
+                                                QWidget *parent) {
     return add(new QPushButton(parent), actionID, components);
 }
 
-QToolButton *ActionController::createToolButton(const QString &actionID, const ActionComponents &components, QWidget *parent) {
+QToolButton *ActionController::createToolButton(const QString &actionID,
+                                                const ActionComponents &components,
+                                                QWidget *parent) {
     return add(new QToolButton(parent), actionID, components);
 }
 
-QCommandLinkButton *ActionController::createCommandLinkButton(const QString &actionID, const ActionComponents &components, QWidget *parent) {
+QCommandLinkButton *ActionController::createCommandLinkButton(const QString &actionID,
+                                                              const ActionComponents &components,
+                                                              QWidget *parent) {
     return add(new QCommandLinkButton(parent), actionID, components);
 }
 
@@ -61,69 +64,12 @@ QStringList ActionController::globalActionIDs() {
     return _globalActions.keys();
 }
 
-void ActionController::onIconSetChanged() {
-    for(QAction *action : _actions.keys()) {
-        if(!_actions[action].second.testFlag(IconComponent))
-            continue;
-
-        QString actionID = _actions[action].first;
-        ActionConfig actionConfig = ActionManager::item(actionID);
-        action->setIcon(IconController::icon(actionConfig.icon));
-    }
-
-    for(QMenu *menu : _menus.keys()) {
-        if(!_menus[menu].second.testFlag(IconComponent))
-            continue;
-
-        QString actionID = _menus[menu].first;
-        ActionConfig actionConfig = ActionManager::item(actionID);
-        menu->setIcon(IconController::icon(actionConfig.icon));
-    }
-
-    for(QAbstractButton *button : _buttons.keys()) {
-        if(!_buttons[button].second.testFlag(IconComponent))
-            continue;
-
-        QString actionID = _buttons[button].first;
-        ActionConfig actionConfig = ActionManager::item(actionID);
-        button->setIcon(IconController::icon(actionConfig.icon));
-    }
-}
-
-void ActionController::onActionShortcutChanged(const QString &keyboardShortcutID, const QKeySequence shortcut) {
-    for(QAction *action : _actions.keys()) {
-        if(_actions[action].first != keyboardShortcutID || !_actions[action].second.testFlag(ShortcutComponent))
-            continue;
-
-        if(action->shortcut() != shortcut)
-            action->setShortcut(shortcut);
-    }
-
-    for(QAbstractButton *button : _buttons.keys()) {
-        if(_buttons[button].first != keyboardShortcutID || !_buttons[button].second.testFlag(ShortcutComponent))
-            continue;
-
-        if(button->shortcut() != shortcut)
-            button->setShortcut(shortcut);
-    }
-
-    if(_globalActions.contains(keyboardShortcutID))
-        _globalActions[keyboardShortcutID].shortcut = shortcut;
-}
-
-
-
-template<typename T>
-T *ActionController::remove(T *widget) {
-    if constexpr(std::is_base_of<QAbstractButton, T>::value)
-        _buttons.remove(widget);
-
-    else if constexpr(std::is_base_of<QAction, T>::value)
-        _actions.remove(widget);
+GlobalActionWrapper ActionController::globalAction(const QString &actionID) {
+    return _globalActions.value(actionID);
 }
 
 void ActionController::setGlobalAction(const QString &actionID, QObject *widget) {
-    if (_globalActions.contains(actionID))
+    if(_globalActions.contains(actionID))
         instance()->disconnect(qobject_cast<QObject *>(_globalActions[actionID].widget));
 
     _globalActions.insert(actionID, widget);
@@ -132,6 +78,55 @@ void ActionController::setGlobalAction(const QString &actionID, QObject *widget)
     });
 }
 
-GlobalActionWrapper ActionController::globalAction(const QString &actionID) {
-    return _globalActions.value(actionID);
+void ActionController::onIconSetChanged() {
+    for(auto it = _actions.begin(); it != _actions.end(); ++it) {
+        if(!it.value().second.testFlag(IconComponent))
+            continue;
+
+        QString actionID = it.value().first;
+        ActionConfig actionConfig = ActionManager::item(actionID);
+        it.key()->setIcon(IconController::icon(actionConfig.icon));
+    }
+
+    for(auto it = _menus.begin(); it != _menus.end(); ++it) {
+        if(!it.value().second.testFlag(IconComponent))
+            continue;
+
+        QString actionID = it.value().first;
+        ActionConfig actionConfig = ActionManager::item(actionID);
+        it.key()->setIcon(IconController::icon(actionConfig.icon));
+    }
+
+    for(auto it = _buttons.begin(); it != _buttons.end(); ++it) {
+        if(!it.value().second.testFlag(IconComponent))
+            continue;
+
+        QString actionID = it.value().first;
+        ActionConfig actionConfig = ActionManager::item(actionID);
+        it.key()->setIcon(IconController::icon(actionConfig.icon));
+    }
+}
+
+void ActionController::onActionShortcutChanged(const QString &keyboardShortcutID,
+                                               const QKeySequence shortcut) {
+    for(auto it = _actions.begin(); it != _actions.end(); ++it) {
+        if(it.value().first != keyboardShortcutID
+           || !it.value().second.testFlag(ShortcutComponent))
+            continue;
+
+        if(it.key()->shortcut() != shortcut)
+            it.key()->setShortcut(shortcut);
+    }
+
+    for(auto it = _buttons.begin(); it != _buttons.end(); ++it) {
+        if(it.value().first != keyboardShortcutID
+           || !it.value().second.testFlag(ShortcutComponent))
+            continue;
+
+        if(it.key()->shortcut() != shortcut)
+            it.key()->setShortcut(shortcut);
+    }
+
+    if(_globalActions.contains(keyboardShortcutID))
+        _globalActions[keyboardShortcutID].shortcut = shortcut;
 }
--- Global/ActionController.h
+++ Global/ActionController.h
@@ -1,70 +1,163 @@
 #ifndef ACTIONCONTROLLER_H
 #define ACTIONCONTROLLER_H
 
-#include <QObject>
-#include <QMenu>
 #include <QCommandLinkButton>
+#include <QMenu>
+#include <QObject>
 #include <QToolButton>
 
 #include "ActionManager.h"
 #include "IconController.h"
 
+/**
+ * @brief The GlobalActionWrapper struct is a wrapper around an action that can be used as a global action.
+ *
+ * It contains a pointer to the action, and the action's shortcut.
+ */
 struct GlobalActionWrapper {
     GlobalActionWrapper(QObject *widget = nullptr);
 
+    /**
+     * @brief Executes the action.
+     *
+     * Depending on the type of the wrapped widget, it calls the click() method
+     * or the trigger() method.
+     */
     void execute();
 
+    /// The widget (usually a QAction or button) that is associated with the global action
     QObject *widget;
+
+    /// The shortcut that is used to trigger the global action
     QKeySequence shortcut;
 };
 
+/**
+ * @brief The ActionController class is responsible for connecting and controlling all actions in the application.
+ *
+ * It connects QActions, buttons etc. to the actions defined in the ActionManager allowing to keep icons, shortcuts etc. up to date.
+ *
+ * It also provides a way to set global actions, which are actions that are present in the application's global command search.
+ *
+ * The ActionController is a singleton class, meaning that it can be accessed from anywhere in the application.
+ */
 class ActionController : public QObject {
     Q_OBJECT
 private:
-    explicit ActionController(QObject *parent);
+    /**
+     * @brief Constructs an ActionController object with the given parent.
+     *
+     * @param parent The parent object.
+     */
+    explicit ActionController(QObject *parent) : QObject(parent) {}
 
 public:
+    /**
+     * @brief Enum for the components of an action.
+     */
     enum ActionComponent {
-        NoComponents = 0x0,
-        IconComponent = 0x1,
-        TextComponent = 0x4,
-        TooltipComponent = 0x8,
-        ShortcutComponent = 0x10,
-        AllComponents = IconComponent | TextComponent | TooltipComponent | ShortcutComponent,
-        AllExceptIconComponent = AllComponents & ~IconComponent,
-        AllExceptTextComponent = AllComponents & ~TextComponent,
-        AllExceptTooltipComponent = AllComponents & ~TooltipComponent,
-        AllExceptShortcutComponent = AllComponents & ~ShortcutComponent,
+        NoComponents = 0x0, ///< No component
+        IconComponent = 0x1, ///< The icon of the action
+        TextComponent = 0x4, ///< The text of the action
+        TooltipComponent = 0x8, ///< The tooltip of the action
+        ShortcutComponent = 0x10, ///< The shortcut of the action
+        AllComponents = IconComponent | TextComponent | TooltipComponent | ShortcutComponent, ///< All components
+        AllExceptIconComponent = AllComponents & ~IconComponent, ///< All components except the icon
+        AllExceptTextComponent = AllComponents & ~TextComponent, ///< All components except the text
+        AllExceptTooltipComponent = AllComponents & ~TooltipComponent, ///< All components except the tooltip
+        AllExceptShortcutComponent = AllComponents & ~ShortcutComponent, ///< All components except the shortcut
     };
     Q_DECLARE_FLAGS(ActionComponents, ActionComponent)
 
+    /**
+     * @brief Initializes the ActionController.
+     */
     static void init();
+
+    /**
+     * @brief Returns the instance of the ActionController.
+     *
+     * @return The instance of the ActionController.
+     */
     static ActionController *instance();
 
-    ActionController& operator=(const ActionController&) = delete;
-    ActionController(const ActionController&) = delete;
+    ActionController &operator=(const ActionController &) = delete;
+    ActionController(const ActionController &) = delete;
 
-    static QAction *createAction(const QString &actionID, const ActionComponents &components = AllComponents, QObject *parent = nullptr);
-    static QMenu *createMenu(const QString &actionID, const ActionComponents &components = AllComponents, QObject *parent = nullptr);
-    static QPushButton *createPushButton(const QString &actionID, const ActionComponents &components = AllComponents, QWidget *parent = nullptr);
-    static QToolButton *createToolButton(const QString &actionID, const ActionComponents &components = AllComponents, QWidget *parent = nullptr);
-    static QCommandLinkButton *createCommandLinkButton(const QString &actionID, const ActionComponents &components = AllComponents, QWidget *parent = nullptr);
+    /**
+     * @brief Creates a new QAction with the given actionID and components.
+     *
+     * @param actionID The ID of the action.
+     * @param components The components of the action to create.
+     * @param parent The parent of the action.
+     * @return The created action.
+     */
+    static QAction *createAction(const QString &actionID,
+                                 const ActionComponents &components = AllComponents,
+                                 QObject *parent = nullptr);
 
+    /**
+     * @brief Creates a QMenu with the given actionID and components.
+     *
+     * @param actionID The ID of the menu.
+     * @param components The components of the menu to create.
+     * @param parent The parent of the menu.
+     * @return The created menu.
+     */
+    static QMenu *createMenu(const QString &actionID,
+                             const ActionComponents &components = AllComponents,
+                             QObject *parent = nullptr);
+
+    /**
+     * @brief Creates a QPushButton with the given actionID and components.
+     *
+     * @param actionID The ID of the button.
+     * @param components The components of the button to create.
+     * @param parent The parent of the button.
+     * @return The created button.
+     */
+    static QPushButton *createPushButton(const QString &actionID,
+                                         const ActionComponents &components = AllComponents,
+                                         QWidget *parent = nullptr);
+
+    /**
+     * @brief Creates a QToolButton with the given actionID and components.
+     *
+     * @param actionID The ID of the button.
+     * @param components The components of the button to create.
+     * @param parent The parent of the button.
+     * @return The created button.
+     */
+    static QToolButton *createToolButton(const QString &actionID,
+                                         const ActionComponents &components = AllComponents,
+                                         QWidget *parent = nullptr);
+
+    /**
+     * @brief Creates a QCommandLinkButton with the given actionID and components.
+     *
+     * @param actionID The ID of the button.
+     * @param components The components of the button to create.
+     * @param parent The parent of the button.
+     * @return The created button.
+     */
+    static QCommandLinkButton *createCommandLinkButton(
+        const QString &actionID, const ActionComponents &components = AllComponents,
+        QWidget *parent = nullptr);
+
+    /**
+     * @brief Adds the given widget as an action to the ActionController.
+     *
+     * @param widget The widget to add.
+     * @param actionID The ID of the action.
+     * @param components The components of the action to add.
+     * @return The added widget.
+     */
     template<typename T>
-    static T *addAsGlobalAction(T *widget,
-                  const QString &actionID,
-                  const ActionComponents &components = AllComponents) {
-        add(widget, actionID, components);
-        setGlobalAction(actionID, widget);
-        return widget;
-    }
-
-    template<typename T>
-    static T *add(T *widget,
-                  const QString &actionID,
+    static T *add(T *widget, const QString &actionID,
                   const ActionComponents &components = AllComponents) {
         if(!ActionManager::itemExists(actionID)) {
-            qWarning() << "cannot add action for actionID" << actionID << "- action does not exist in ActionManager.";
+            qWarning() << "cannot add action for actionID" << actionID
+                       << "- action does not exist in ActionManager.";
             return widget;
         }
 
@@ -73,7 +166,9 @@ public:
         if constexpr(std::is_base_of<QAbstractButton, T>::value) {
             QAbstractButton *button = static_cast<QAbstractButton *>(widget);
             _buttons.insert(button, QPair<QString, ActionComponents>(actionID, components));
-            connect(button, &QObject::destroyed, instance(), [button](){ _buttons.remove(button); });
+            connect(button, &QObject::destroyed, instance(), [button]() {
+                _buttons.remove(button);
+            });
 
             if(components.testFlag(TextComponent))
                 button->setText(actionConfig.text);
@@ -92,7 +187,9 @@ public:
         } else if constexpr(std::is_base_of<QAction, T>::value) {
             QAction *action = static_cast<QAction *>(widget);
             _actions.insert(action, QPair<QString, ActionComponents>(actionID, components));
-            connect(action, &QObject::destroyed, instance(), [action](){ _actions.remove(action); });
+            connect(action, &QObject::destroyed, instance(), [action]() {
+                _actions.remove(action);
+            });
 
             if(components.testFlag(TextComponent))
                 action->setText(actionConfig.text);
@@ -111,7 +208,7 @@ public:
         } else if constexpr(std::is_base_of<QMenu, T>::value) {
             QMenu *menu = static_cast<QMenu *>(widget);
             _menus.insert(menu, QPair<QString, ActionComponents>(actionID, components));
-            connect(menu, &QObject::destroyed, instance(), [menu](){ _menus.remove(menu); });
+            connect(menu, &QObject::destroyed, instance(), [menu]() { _menus.remove(menu); });
 
             if(components.testFlag(TextComponent))
                 menu->setTitle(actionConfig.text);
@@ -122,21 +219,77 @@ public:
             if(components.testFlag(IconComponent))
                 menu->setIcon(IconController::icon(actionConfig.icon));
         } else {
-            qWarning() << "Cannot add action to ActionController. Unsupported type:" << typeid(T).name();
+            qWarning() << "Cannot add action to ActionController. Unsupported type:"
+                       << typeid(T).name();
         }
 
         return widget;
     }
 
-    template <typename T>
-    static T *remove(T *widget);
+    /**
+     * @brief Adds the given widget (usually a QAction or button) as a global action to the ActionController.
+     *
+     * This is a convenience function that adds the widget as an action to the
+     * ActionController and sets it as a global action. The widget is added with
+     * the given actionID and components.
+     *
+     * This means that when the action specified by actionID is called from the global command search,
+     * the widget will be clicked or triggered.
+     *
+     * This is unique which means, if another widget is added with the same actionID, the previous widget will be removed.
+     *
+     * @param widget The widget to add.
+     * @param actionID The ID of the action.
+     * @param components The components of the action to add.
+     * @return The added widget.
+     */
+    template<typename T>
+    static T *addAsGlobalAction(T *widget, const QString &actionID,
+                                const ActionComponents &components = AllComponents) {
+        add(widget, actionID, components);
+        setGlobalAction(actionID, widget);
+        return widget;
+    }
 
+    /**
+     * @brief Removes the given widget from the ActionController.
+     *
+     * @param widget The widget to remove.
+     * @return The removed widget.
+     */
+    template<typename T>
+    static T *remove(T *widget) {
+        if constexpr(std::is_base_of<QAbstractButton, T>::value)
+            _buttons.remove(widget);
+
+        else if constexpr(std::is_base_of<QAction, T>::value)
+            _actions.remove(widget);
+    }
+
+    /**
+     * @brief Returns the IDs of all global actions.
+     *
+     * @return The IDs of all global actions.
+     */
     static QStringList globalActionIDs();
 
+    /**
+     * @brief Returns the global action with the given actionID.
+     *
+     * @param actionID The ID of the global action.
+     * @return The global action with the given actionID.
+     */
     static GlobalActionWrapper globalAction(const QString &actionID);
 
+    /**
+     * @brief Sets the global action with the given actionID to the given widget.
+     * 
+     * This is unique which means, if another widget is added with the same actionID, the previous widget will be removed.
+     *
+     * @param actionID The ID of the global action.
+     * @param widget The widget to set as the global action.
+     */
     static void setGlobalAction(const QString &actionID, QObject *widget);
-
 
 protected slots:
     void onIconSetChanged();
