0e3ef7abacfa61c0a168aada619865c9d5bfe6e0
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2025-06-06 12:34:19 +0200

    CrashDetector: refactoring & documentation

--- Global/CrashDetector.cpp
+++ Global/CrashDetector.cpp
@@ -2,21 +2,13 @@
 
 #include "SettingsManager.h"
 
-CrashDetector::CrashDetector(QObject *parent) : QObject(parent) {
-    _lastCrashDetected = !SettingsManager::value("general.closeCheck").toBool();
-    SettingsManager::setValue("general.closeCheck", false);
-}
-
 CrashDetector::~CrashDetector() {
     SettingsManager::setValue("general.closeCheck", true);
 }
 
-CrashDetector *CrashDetector::instance() {
-    static CrashDetector instance(nullptr);
-    return &instance;
-}
-
 void CrashDetector::init() {
+    _lastCrashDetected = !SettingsManager::value("general.closeCheck").toBool();
+    SettingsManager::setValue("general.closeCheck", false);
     instance();
 }
 
--- Global/CrashDetector.h
+++ Global/CrashDetector.h
@@ -3,25 +3,49 @@
 
 #include <QObject>
 
-class CrashDetector : public QObject {
+#include "Global/Singleton.h"
+
+/**
+ * @class CrashDetector
+ * @brief The CrashDetector class is responsible for detecting if the application crashed in the last session.
+ *
+ * This class provides a singleton instance to check for a crash occurrence.
+ * It utilizes the SettingsManager to manage the crash detection.
+ */
+class CrashDetector : public Singleton<CrashDetector> {
+    friend class Singleton<CrashDetector>;
     Q_OBJECT
+    Q_DISABLE_COPY_MOVE(CrashDetector)
+
 private:
-    explicit CrashDetector(QObject *parent);
+    /**
+     * @brief Constructs a CrashDetector object.
+     * @param parent The parent QObject.
+     */
+    CrashDetector() : Singleton<CrashDetector>() {}
+
+    /**
+     * @brief Destroys the CrashDetector object.
+     *
+     * This saves that the application was closed normally and didn't crash.
+     */
     ~CrashDetector();
 
 public:
-    static CrashDetector *instance();
+    /**
+     * @brief Initializes the CrashDetector instance.
+     */
     static void init();
 
-    CrashDetector(const CrashDetector &other) = delete;
-    CrashDetector &operator=(const CrashDetector &other) = delete;
-
+    /**
+     * @brief Returns if a crash was detected.
+     * @return True if a crash was detected, otherwise false.
+     */
     static bool crashDetected();
 
-signals:
-
 private:
-    static inline bool _lastCrashDetected = false;
+    static inline bool _lastCrashDetected = false; ///< Indicates the last crash detection status.
 };
 
 #endif // CRASHDETECTOR_H
+
