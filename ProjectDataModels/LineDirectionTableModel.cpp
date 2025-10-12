e9eed21e94cdb2e339353260550e0719b43edfc7
ma7t3 <104677051+ma7t3@users.noreply.github.com>
2025-10-12 12:55:27 +0200

    Fix invalid connect calls

--- ProjectDataModels/LineDirectionTableModel.cpp
+++ ProjectDataModels/LineDirectionTableModel.cpp
@@ -9,11 +9,13 @@ void LineDirectionTableModel::setLine(Line *line) {
 
     _line = line;
     reset();
-    connect(_line, &Line::directionAdded,   this, &LineDirectionTableModel::onItemInserted);
-    connect(_line, &Line::directionRemoved, this, &LineDirectionTableModel::onItemRemoved);
-    connect(_line, &Line::directionChanged, this, &LineDirectionTableModel::onItemUpdated);
-    connect(_line, &Line::directionMoved,   this, &LineDirectionTableModel::onItemMoved);
-    connect(_line, &QObject::destroyed,     this, [this]() { setLine(nullptr); });
+    if(_line) {
+        connect(_line, &Line::directionAdded,   this, &LineDirectionTableModel::onItemInserted);
+        connect(_line, &Line::directionRemoved, this, &LineDirectionTableModel::onItemRemoved);
+        connect(_line, &Line::directionChanged, this, &LineDirectionTableModel::onItemUpdated);
+        connect(_line, &Line::directionMoved,   this, &LineDirectionTableModel::onItemMoved);
+        connect(_line, &QObject::destroyed,     this, [this]() { setLine(nullptr); });
+    }
 }
 
 int LineDirectionTableModel::columnCount(const QModelIndex &parent) const {
